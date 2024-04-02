22f5994a811ffa325b2757caa0d254155d0bdab1
Sushil <sushilkumar811544@gmail.com>
2024-04-02 17:05:29 +0530

    useState...Hook used to apply filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,17 +3,28 @@ import { useState } from "react";
 import resList from "../utils/mockData";
 
 const Body = () => {
+    const [listOfRestaurants,setListOfRestaurants] = useState(resList);
     return (
        <div className="body">
-           <div className="search">Search</div>
+           <div className="filter">
+            <button className="filter-btn"
+            onClick={()=>{
+                 const filteredData = listOfRestaurants.filter(
+                    (res)=>res.info.avgRatingString>4.3
+                );
+                setListOfRestaurants(filteredData);
+            }}
+            >
+                Top Rated Restaurants
+            </button>
+            </div>
 
            <div className="res-container">
-             
-               {resList.map((restaurant) => (
+               {listOfRestaurants.map((restaurant) => (
                    <RestaurantCard key = {restaurant.info.id} resData = {restaurant}/>
                ))}
            </div>
        </div>
-    )
-}
+    );
+};
 export default Body;
