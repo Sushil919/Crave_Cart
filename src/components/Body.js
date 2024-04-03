79b5422259414770f4e073d70bd9ba9489f7f973
Sushil <sushilkumar811544@gmail.com>
2024-04-03 18:45:39 +0530

    Got live data using fetch API..😎✌🚀😍got rid of mock data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,24 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import resList from "../utils/mockData";
+import { useEffect, useState } from "react";
 
 const Body = () => {
-    const [listOfRestaurants,setListOfRestaurants] = useState(resList);
+    const [listOfRestaurants,setListOfRestaurants] = useState([]);
+
+    useEffect(()=>{
+        fetchData()
+    },
+    []);
+
+    const fetchData = async () => {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.7605545&lng=83.3731675&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
+        const json = await data.json();
+
+        console.log(json);
+            //Optional chaining : right way to write code
+        setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    };
     return (
        <div className="body">
            <div className="filter">
