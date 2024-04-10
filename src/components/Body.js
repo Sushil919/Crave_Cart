6a55e45a4ffb905728f7eda296eb31abb581de02
Sushil <sushilkumar811544@gmail.com>
2024-04-10 16:37:05 +0530

    Revert "created another state variable for the filtered res and kept all the listOf'Res as it fetched through API"
    This reverts commit e6ce0a1958019ed717fea8ffd51206d8e8a0f77a.

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,6 @@ import Shimmer from "./Shimmer";
 
 const Body = () => {
     const [listOfRestaurants,setListOfRestaurants] = useState([]);
-    const [filteredRestaurants,setFilteredRestaurants] = useState([]);
     const [searchText, setSearchText] = useState("");
 
     useEffect(()=>{
@@ -21,7 +20,6 @@ const Body = () => {
         console.log(json);
         
         setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-        setFilteredRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
     };
     return listOfRestaurants.length === 0 ? (<Shimmer /> ) : (
        <div className="body">
@@ -39,7 +37,7 @@ const Body = () => {
                             const filteredRestaurant = listOfRestaurants.filter(
                                 (res) => res.info.name.toLowerCase().includes(searchText.toLowerCase())
                             );
-                            setFilteredRestaurants(filteredRestaurant);
+                            setListOfRestaurants(filteredRestaurant);
                         }
                     }>
                         Search
@@ -58,7 +56,7 @@ const Body = () => {
             </div>
 
            <div className="res-container">
-               {filteredRestaurants.map((restaurant) => (
+               {listOfRestaurants.map((restaurant) => (
                    <RestaurantCard key = {restaurant.info.id} resData = {restaurant}/>
                ))}
            </div>
